26faf1578fa584fd0d1c14632f50b884ad130171
Franco Gilli <francogilli10@gmail.com>
2024-01-29 16:22:31 -0300

    Error persist

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,24 +1,10 @@
 // app/components/ThemeSwitcher.tsx
 "use client";
 
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
-  if (!mounted) return null;
+      return <a href="#"  className="cursor-pointer text-zinc-200 hover:text-zinc-100">a</a>
 
-  if (theme==='dark'){
-      return <a href="#" onClick={() => setTheme("light")} className="cursor-pointer text-zinc-200 hover:text-zinc-100">a</a>
-  }
-
-  if(theme==='light'){
-    return <a href="#" onClick={() => setTheme("dark")} className="cursor-pointer text-zinc-800 hover:text-zinc-900">b</a>
-  }
   
 }
