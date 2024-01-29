fbbe9d8851a10ab7dc0fa053890fc848e9cbe93a
Franco Gilli <francogilli10@gmail.com>
2024-01-29 17:04:14 -0300

    Fix: next-themes in dark mode

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,5 +4,5 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{children}</ThemeProvider>
+    return <ThemeProvider attribute="class" defaultTheme='system' enableSystem disableTransitionOnChange>{children}</ThemeProvider>
 }
--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -2,7 +2,6 @@
 import { Icon } from '@iconify/react';
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
-import Image from "next/image"
 
 export default function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false)
@@ -10,24 +9,14 @@ export default function ThemeSwitcher() {
 
   useEffect(() =>  setMounted(true), [])
 
-  if (!mounted) return (
-    <Image
-      src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-      width={36}
-      height={36}
-      sizes="36x36"
-      alt="Loading Light/Dark Toggle"
-      priority={false}
-      title="Loading Light/Dark Toggle"
-    />
-  )
+  if (!mounted) return null;
 
   if (resolvedTheme === 'dark') {
-    return <Icon icon="lucide:sun" width="20" height="20" onClick={() => setTheme('light')} />
+    return <Icon icon="lucide:moon" className="cursor-pointer text-zinc-400 hover:text-zinc-100" width="20" height="20" onClick={() => setTheme('light')} />
   }
 
   if (resolvedTheme === 'light') {
-    return <Icon icon="lucide:moon" width="20" height="20" onClick={() => setTheme('dark')} />
+    return <Icon icon="lucide:sun" className="cursor-pointer text-zinc-800 hover:text-zinc-900" width="20" height="20" onClick={() => setTheme('dark')} />
   }
 
 }
