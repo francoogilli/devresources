515c6d6796984d35ad9fdd14b8380b4d152a29b7
Franco Gilli <francogilli10@gmail.com>
2024-01-30 17:18:15 -0300

    Add new icons for dark mode

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -12,11 +12,11 @@ export default function ThemeSwitcher() {
   if (!mounted) return null;
 
   if (resolvedTheme === 'dark') {
-    return <Icon icon="lucide:moon" className="cursor-pointer text-zinc-300 hover:text-zinc-100" width="20" height="20" onClick={() => setTheme('light')} />
+    return <Icon icon="radix-icons:moon" className="cursor-pointer text-zinc-300 hover:text-zinc-100" width="20" height="20" onClick={() => setTheme('light')} />
   }
 
   if (resolvedTheme === 'light') {
-    return <Icon icon="lucide:sun" className="cursor-pointer text-zinc-800 hover:text-zinc-900" width="20" height="20" onClick={() => setTheme('dark')} />
+    return <Icon icon="radix-icons:sun" className="cursor-pointer text-zinc-800 hover:text-zinc-900" width="20" height="20" onClick={() => setTheme('dark')} />
   }
 
 }
