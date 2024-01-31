a483275bf71c6eb5e0cf720e583ae941299237da
Franco Gilli <francogilli10@gmail.com>
2024-01-31 10:14:03 -0300

    Update important section in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import {Icon} from '@iconify/react';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center pt-10 md:pt-16 bg-white dark:bg-neutral-950">
@@ -6,17 +8,20 @@ export default function Home() {
         <div className="shrink-0 bg-border w-[1px] mx-2 h-4"></div>    
         <span>Nuevos recursos, actualizados y más.</span>
       </span>
-      <h1 className="font-bold text-4xl text-center md:text-6xl mb-5  text-black dark:text-white">
-      ¡Libreria de recursos para 
-      <span className="tracking-tight inline from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b"> devs</span>
-      !</h1>
+
+      <h1 className="font-bold text-4xl text-center md:text-6xl mb-5 text-black dark:text-white">
+        ¡Biblioteca de recursos para 
+        <span className="tracking-tight inline from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b"> devs</span>
+        !
+      </h1>
       
       <div className="font-normal text-center w-full max-w-6xl px-4 py-0 rounded-lg bg-white dark:bg-neutral-950 mx-auto">
-        <div className="max-w-2xl mx-auto pb-5">
+        <div className="max-w-2xl mx-auto pb-10">
           <span className="text-lg sm:text-xl mb-6 font-normal text-black dark:text-gray-400">
             Recursos cuidadosamente seleccionados para potenciar tus proyectos. Accesibles. Versatiles. De código abierto.
           </span>
         </div>
+
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 font-medium mb-10">
           <div className="flex items-center justify-center p-4 bg-black/5 dark:bg-white/5 rounded-2xl md:rounded-3xl">
             <p className="text-lg sm:text-xl text-black dark:text-white">
@@ -34,14 +39,33 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <span className="tracking-tight inline font-bold text-4xl md:text-5xl from-[#e9ef5e] to-[#ebf500] bg-clip-text text-transparent bg-gradient-to-b">
-          !important
+        <span className="flex items-center justify-center gap-x-3 tracking-tight font-bold text-4xl md:text-5xl from-[#1b1b1b] to-[#000000] dark:from-[#ffffff] dark:to-[#b3b4b0] bg-clip-text text-transparent bg-gradient-to-b">
+          <Icon icon="mdi:warning-circle" className='text-blue-500 dark:text-blue-500 rotate-180'  />README
         </span>
-        <div className="mt-4 rounded-3xl ">
-          <p className="text-base sm:text-lg mb-4 text-black dark:text-gray-300">📌 ¿Crees que debería haber un recurso aquí y no lo encuentras? ¡Haz un pull request y añádela vos mismo!</p>
-          <p className="text-base sm:text-lg mb-4 text-black dark:text-gray-300">😨 Si no te sientes seguro, no hay problema. ¡Simplemente abre un issue con la opción &quot;Agregar nuevo recurso&quot; y nosotros lo agregaremos por vos!</p>
-          <p className="text-base sm:text-lg text-black dark:text-gray-300">🤔 ¿Te gustaría tener videos tutoriales para algunas herramientas? ¡Genial! Abre un issue con la opción &quot;Agregar nuevo video para recurso&quot; y haremos todo lo posible para proporcionarlo.</p>
-        </div>
+    <div className="mt-4 rounded-3xl max-w-5xl mx-auto bg-gray-100 dark:bg-neutral-900 p-6">
+      <div className='flex flex-col items-center pb-5'>
+        <Icon icon="simple-icons:github" className='mb-5 text-zinc-900 dark:text-white' width={60} height={60} />
+        <h2 className="text-2xl md:text-3xl font-bold text-center mb-4 ">
+          ¡Contribuye al proyecto de Resources Dev!
+        </h2>
+      </div>
+  <div className="flex flex-col space-y-4 text-base md:text-lg">
+    <div className="flex items-center space-x-2">
+      <Icon icon="octicon:issue-opened-16" className="text-black dark:text-white" width="16" height="16"/>
+      <p className=" text-black dark:text-gray-300">¿Tienes alguna idea de recurso que te gustaría ver aquí? ¡Haz un pull request!</p>
+    </div>
+    <div className="flex items-center space-x-2">
+      <Icon icon="octicon:issue-opened-16" className="text-black dark:text-white" width="16" height="16"/>
+      <p className=" text-black dark:text-gray-300">¿No te sientes seguro? ¡Abre un issue y te ayudaremos a agregarlo!</p>
+    </div>
+    <div className="flex items-center space-x-2">
+      <Icon icon="octicon:issue-opened-16" className="text-black dark:text-white" width="16" height="16"/>
+      <p className=" text-black dark:text-gray-300">Toda recomendacion, mejora o cambios sugeridos son bienvenidos.</p>
+    </div>
+  </div>
+</div>
+
+
       </div>
     </div>
   );
