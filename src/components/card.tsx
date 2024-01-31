3facce64911b13973935415d26dc413792eed361
Franco Gilli <francogilli10@gmail.com>
2024-01-31 20:06:31 -0300

    fix: Replace <img> with <Image /> for image optimization

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,5 @@
-// card.tsx
 import React from "react";
+import Image from 'next/image';
 
 interface CardProps {
   title: string;
@@ -13,8 +13,8 @@ const Card: React.FC<CardProps> = ({ title, link, iconDark, iconLight }) => {
     <a href={link} target="_blank" rel="noopener noreferrer">
       <article className="relative flex items-center justify-center overflow-hidden rounded-3xl border border-neutral-200 dark:border-neutral-800 bg-gray-100 hover:bg-zinc-200/60 dark:bg-neutral-900/60 dark:hover:bg-neutral-900 cursor-pointer">
         <div className="flex flex-col items-center justify-center rounded-md p-4">
-        <img src={iconDark} alt={title} className="hidden dark:block mb-4 mt-2 w-20 h-20" />
-        <img src={iconLight} alt={title} className="block dark:hidden mb-4 mt-2 w-20 h-20" />
+        <Image src={iconDark} alt={title} className="hidden dark:block mb-4 mt-2 w-20 h-20" width={80} height={80} />
+        <Image src={iconLight} alt={title} className="block dark:hidden mb-4 mt-2 w-20 h-20" width={80} height={80} />
           <div className="flex flex-col space-y-1 items-center justify-center">
             <p className="truncate text-[19px] font-semibold text-balance text-center select-all">{title}</p>
           </div>
