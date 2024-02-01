75053d7a652dea502c45b94c8b5713d89b257527
Franco Gilli <francogilli10@gmail.com>
2024-02-01 10:53:19 -0300

    Add Frontend color palettes data

--- src/app/frontend/colors/page.tsx
+++ src/app/frontend/colors/page.tsx
@@ -4,7 +4,7 @@ import { colors } from "src/data/frontend/colors";
 const ColorsPage = ()=>{
     return (
         <>
-            <SectionPage title={colors.title} category={colors.category} subtitle={colors.subtitle} color1={colors.color1} color2={colors.color2}/>
+            <SectionPage title={colors.title} category={colors.category} subtitle={colors.subtitle} color1={colors.color1} color2={colors.color2} resources={colors.resources}/>
         </>
     )
 }
--- src/data/frontend/colors.ts
+++ src/data/frontend/colors.ts
@@ -4,4 +4,32 @@ export const colors = {
     subtitle: 'Paletas de colores para usar en tus proyectos o portafolio.',
     color1: '#00adff',
     color2: '#004aff',
+    resources: [
+        {
+            title:'Adobe Color',
+            link:'https://color.adobe.com/es/create/color-wheel/',
+            iconDark: '/icons/Adobecolor_dark.svg',
+            iconLight: '/icons/Adobecolor_light.svg'
+        },
+        {
+            title:'Coolors',
+            link:'https://coolors.co/',
+            iconDark: '/icons/coolors.svg',
+            iconLight: '/icons/coolors.svg'
+        },
+        {
+            title:'Happy Hues',
+            link:'https://www.happyhues.co/',
+            iconDark: '/icons/happyhues.svg',
+            iconLight: '/icons/happyhues.svg'
+        },
+        {
+            title:'UI Colors',
+            link:'https://uicolors.app/',
+            iconDark: '/icons/Uicolors_dark.svg',
+            iconLight: '/icons/Uicolors_light.svg'
+        }
+    ]
 }
+
+
