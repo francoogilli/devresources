33aa5ee2b4cfd42b97cd49468c0eee58dbd1189f
Franco Gilli <francogilli10@gmail.com>
2024-02-01 09:00:40 -0300

    Add Frontend icons data

--- src/app/frontend/iconos/page.tsx
+++ src/app/frontend/iconos/page.tsx
@@ -4,7 +4,7 @@ import { icons } from "src/data/frontend/icons";
 const IconsPage = ()=>{
     return (
         <>
-            <SectionPage title={icons.title} category={icons.category} subtitle={icons.subtitle} color1={icons.color1} color2={icons.color2}/>
+            <SectionPage title={icons.title} category={icons.category} subtitle={icons.subtitle} color1={icons.color1} color2={icons.color2} resources={icons.resources}/>
         </>
     )
 }
--- src/data/frontend/icons.ts
+++ src/data/frontend/icons.ts
@@ -4,4 +4,37 @@ export const icons = {
     subtitle: 'Iconos para utilizar en tus proyectos o portafolio.',
     color1: '#00adff',
     color2: '#004aff',
+    resources: [
+        {
+            title:'Svgl',
+            link:'https://svgl.app/',
+            iconDark: '/icons/svgl.svg',
+            iconLight: '/icons/svgl.svg'
+        },
+        {
+            title:'Tabler Icons',
+            link:'https://tabler.io/icons',
+            iconDark: '/icons/tablerio.svg',
+            iconLight: '/icons/tablerio.svg'
+        },
+        {
+            title:'Lucide',
+            link:'https://lucide.dev/',
+            iconDark: '/icons/Lucide_dark.svg',
+            iconLight: '/icons/Lucide_light.svg'
+        },
+        {
+            title:'Ionicons',
+            link:'https://ionicons.com/',
+            iconDark: '/icons/ionicons.svg',
+            iconLight: '/icons/ionicons.svg'
+        },
+        {
+            title:'Heroicons',
+            link:'https://heroicons.com/',
+            iconDark: '/icons/heroicons.svg',
+            iconLight: '/icons/heroicons.svg'
+        }
+
+    ]
 }
