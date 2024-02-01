d1b2ea75481658ea9d4669dd52009c9a0f2cc9bd
Franco Gilli <francogilli10@gmail.com>
2024-01-31 23:27:28 -0300

    Add Backend deploys data

--- src/data/backend/deploys.ts
+++ src/data/backend/deploys.ts
@@ -4,4 +4,36 @@ export const deploys = {
     subtitle: 'Sitios para deployar tu proyecto de backend.',
     color1: '#FF705B',
     color2: '#FFB457',
+    resources: [
+        {
+            title:'Firebase',
+            link:'https://firebase.google.com',
+            iconDark:'/icons/firebase.svg',
+            iconLight: '/icons/firebase.svg'
+        },
+        {
+            title:'Koyeb',
+            link:'https://koyeb.com',
+            iconDark:'/icons/Koyeb_dark.svg',
+            iconLight: '/icons/Koyeb_light.svg'
+        },
+        {
+            title:'Railway',
+            link:'https://railway.app',
+            iconDark:'/icons/Railway_dark.svg',
+            iconLight: '/icons/Railway_light.svg'
+        },
+        {
+            title:'Fly.io',
+            link:'https://fly.io',
+            iconDark:'/icons/fly.svg',
+            iconLight: '/icons/fly.svg'
+        },
+        {
+            title:'NHost',
+            link:'https://nhost.io',
+            iconDark:'/icons/nhost.svg',
+            iconLight: '/icons/nhost.svg'
+        }
+    ]
 }
