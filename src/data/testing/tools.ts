8872f47cbb27015b279c1867858f13e143410745
Franco Gilli <francogilli10@gmail.com>
2024-01-31 22:38:46 -0300

    Add Testing tools data

--- src/data/testing/tools.ts
+++ src/data/testing/tools.ts
@@ -4,4 +4,30 @@ export const tools = {
     subtitle: 'Distintos frameworks o herramientas para testear tu código.',
     color1: '#00cc51',
     color2: '#00981e',
+    resources: [
+        {
+            title:'Vitest',
+            link:'https://vitest.dev/',
+            iconDark: '/icons/vitest.svg',
+            iconLight: '/icons/vitest.svg'
+        },
+        {
+            title: 'Jest',
+            link: 'https://jestjs.io/',
+            iconDark: '/icons/jest.svg',
+            iconLight: '/icons/jest.svg'
+        },
+        {
+            title: 'Cypress',
+            link: 'https://www.cypress.io/',
+            iconDark: '/icons/cypress.svg',
+            iconLight: '/icons/cypress.svg'
+        },
+        {
+            title: 'Playwright',
+            link: 'https://playwright.dev/',
+            iconDark: '/icons/playwright.svg',
+            iconLight: '/icons/playwright.svg'
+        }
+    ]
 }
