695e846e2d86093c72d55847bdfe7bd087eac845
Franco Gilli <francogilli10@gmail.com>
2024-02-02 10:14:44 -0300

    Add Backend databases data

--- src/data/backend/databases.ts
+++ src/data/backend/databases.ts
@@ -4,4 +4,30 @@ export const databases = {
     subtitle: 'Recursos sobre diferentes tipos de bases de datos, modelos de datos, esquemas, consultas comunes, etc.',
     color1: '#FF705B',
     color2: '#FFB457',
+    resources: [
+        {
+            title:'PostgreSQL',
+            link:'https://www.postgresql.org/',
+            iconDark:'/icons/postgresql.svg',
+            iconLight: '/icons/postgresql.svg'
+        },
+        {
+            title:'MongoDB',
+            link:'https://www.mongodb.com/',
+            iconDark:'/icons/mongodb.svg',
+            iconLight: '/icons/mongodb.svg'
+        },
+        {
+            title:'MySQL',
+            link:'https://www.mysql.com/',
+            iconDark:'/icons/mysql.svg',
+            iconLight: '/icons/mysql.svg'
+        },
+        {
+            title:'TypeORM',
+            link:'https://typeorm.io/',
+            iconDark:'/icons/typeorm.svg',
+            iconLight: '/icons/typeorm.svg'
+        }
+    ]
 }
